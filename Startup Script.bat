c6452d07fc724777488e604a632ffd6b4245f083
iazharh <60356502+iazharh@users.noreply.github.com>
2020-08-25 20:36:45 +0530

    Add files via upload

---
+++ Startup Script.bat
@@ -0,0 +1,1 @@
+PowerShell.exe -ExecutionPolicy Bypass -File "D:\Let's Automate\Scripts\Startup Script.ps1"
---
+++ Startup Script.ps1
@@ -0,0 +1,32 @@
+﻿# Authenticate on CR
+$Url = "http://doctorstrange/v1/authentication"
+$Body = ConvertTo-Json @{
+  #Username and password which have access to the locker
+  username = "testuser"
+    password = "testpassword"
+ }
+$response = Invoke-RestMethod -Method Post -ContentType application/json -Uri $Url -Body $Body
+
+# Get Credential ID for the PC
+$Uri = "http://doctorstrange/v2/credentialvault/credentials?name=$env:computername"
+$kill = @{
+Headers = @{'X-Authorization'= $response.token}
+Method = 'Get'
+ContentType = 'application/json'
+}
+
+#Get credentials attributes into variable
+$getcredid = Invoke-RestMethod -Uri $Uri @kill
+$credid = $getcredid.list[0].id
+$Uri = "http://doctorstrange/v2/credentialvault/credentials/$credid/attributevalues"
+$clientcreds = @{
+Headers = @{'X-Authorization'= $response.token}
+Method = 'Get'
+ContentType = 'application/json'
+}
+$clientcredentials = Invoke-RestMethod -Uri $Uri @clientcreds
+$username = $clientcredentials.list[0].value
+$password = $clientcredentials.list[1].value
+
+#Login to AA using credentials attribute
+& "C:\Program Files (x86)\Automation Anywhere\Enterprise\Client\Automation Anywhere.exe" "/u$username" "/p$password" "/aopen" "/llogin"
